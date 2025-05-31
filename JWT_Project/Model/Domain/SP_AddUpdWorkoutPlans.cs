bc3db429062d1912ab5ea0bb05485ca4462e51b6
Sanket Chaudhari <sanketjchaudhari@gmail.com>
2025-05-31 12:58:48 +0530

    changes

--- JWT_Project/Model/Domain/SP_AddUpdWorkoutPlans.cs
+++ JWT_Project/Model/Domain/SP_AddUpdWorkoutPlans.cs
@@ -5,8 +5,8 @@
         public int ID			 { get; set; }
         public string PLANNAME	 { get; set; }
         public string DESCRIPTION { get; set; }
-        public string TRAINERID	 { get; set; }
-        public string MEMBERID	 { get; set; }
+        public int TRAINERID	 { get; set; }
+        public int MEMBERID	 { get; set; }
         public string CREATEDAT { get; set; }
     }
 }
