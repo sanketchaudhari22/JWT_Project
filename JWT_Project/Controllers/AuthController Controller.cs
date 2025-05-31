efd0ecfca9ab636c93b9e45e27659fababe94ace
Sanket Chaudhari <sanketjchaudhari@gmail.com>
2025-05-31 16:31:42 +0530

    add api

---
+++ JWT_Project/Controllers/AuthController Controller.cs
@@ -0,0 +1,39 @@
+﻿using JWT_Project.Model.Domain;
+using JWT_Project.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserLogin user)
+        {
+            if (user.Username == "admin" && user.Password == "admin123")
+            {
+                var token = _tokenService.GenerateToken(user.Username);
+                return Ok(new { token });
+            }
+
+            return Unauthorized("Wrong credentials 😵");
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public IActionResult Profile()
+        {
+            var username = User.Identity.Name;
+            return Ok(new { message = $"Welcome back {username} 🔥" });
+        }
+    }
+}
---
+++ JWT_Project/Repository/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_Project.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
